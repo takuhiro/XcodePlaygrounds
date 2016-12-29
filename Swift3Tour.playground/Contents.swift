598e74c15051dfc35ef35b001ec94aead3e015e1
Takuhiro Yoshida <takuhiro@users.noreply.github.com>
2016-12-30 08:34:09 +0900

    Try willSet.

--- Swift3Tour.playground/Contents.swift
+++ Swift3Tour.playground/Contents.swift
@@ -325,8 +325,8 @@ class EquilateralTriangle: NamedShape {
         get {
             return 3.0 * sideLength
         }
-        set {
-            sideLength = newValue / 3.0
+        set(v) {
+            sideLength = v / 3.0
         }
     }
     
@@ -339,4 +339,30 @@ var triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
 print(triangle.perimeter)
 triangle.perimeter = 9.9
 print(triangle.sideLength)
+
+
+// willSet
+
+class TriangleAndSquare {
+    var triangle: EquilateralTriangle {
+        willSet {
+            square.sideLength = newValue.sideLength
+        }
+    }
+    var square: Square {
+        willSet {
+            triangle.sideLength = newValue.sideLength
+        }
+    }
+    init(size: Double, name: String) {
+        square = Square(sideLength: size, name: name)
+        triangle = EquilateralTriangle(sideLength: size, name: name)
+    }
+}
+
+var triangleAndSquare = TriangleAndSquare(size: 10, name: "another test shape")
+print(triangleAndSquare.square.sideLength)
+print(triangleAndSquare.triangle.sideLength)
+triangleAndSquare.square = Square(sideLength: 50, name: "larger square")
+print(triangleAndSquare.triangle.sideLength)
 
