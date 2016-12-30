c907b70e58d616e58e42d8afbb4733bf03fc3474
Takuhiro Yoshida <takuhiro@users.noreply.github.com>
2016-12-30 12:23:39 +0900

    Error Handling.

--- Swift3Tour.playground/Contents.swift
+++ Swift3Tour.playground/Contents.swift
@@ -577,3 +577,64 @@ extension Double {
 let protocolValue: ExampleProtocol = a
 print(protocolValue.simpleDescription)
 //print(protocolValue.anotherProperty)
+
+
+// Error Handling
+
+enum PrinterError: Error {
+    case outOfPaper
+    case noToner
+    case onFire
+}
+
+func send(job: Int, toPrinter printerName: String) throws -> String {
+    if printerName == "Never Has Toner" {
+        throw PrinterError.noToner
+    } else if printerName == "on fire" {
+        throw PrinterError.onFire
+    } else if printerName == "out of paper" {
+        throw PrinterError.outOfPaper
+    }
+    return "Job sent"
+}
+
+do {
+//    let printerResponse = try send(job: 1040, toPrinter: "Bi Sheng")
+    let printerResponse = try send(job: 1040, toPrinter: "Never Has Toner")
+    print(printerResponse)
+} catch {
+    print(error)
+}
+
+do {
+//    let printerResponse = try send(job: 1440, toPrinter: "Gutenberg")
+//    let printerResponse = try send(job: 1440, toPrinter: "on fire")
+    let printerResponse = try send(job: 1440, toPrinter: "out of paper")
+    print(printerResponse)
+} catch PrinterError.onFire {
+    print("I'll just put this over here, with the rest of the fire.")
+} catch let printerError as PrinterError {
+    print("Printer error: \(printerError).")
+} catch {
+    print(error)
+}
+
+let printerSuccess = try? send(job: 1884, toPrinter: "Mergenthaler")
+let printerFailure = try? send(job: 1885, toPrinter: "Never Has Toner")
+
+// defer
+
+var fridgeIsOpen = false
+let fridgeContent = ["milk", "eggs", "leftovers"]
+
+func fridgeContains(_ food: String) -> Bool {
+    fridgeIsOpen = true
+    defer {
+        fridgeIsOpen = false
+    }
+    
+    let result = fridgeContent.contains(food)
+    return result
+}
+fridgeContains("banana")
+print(fridgeIsOpen)
